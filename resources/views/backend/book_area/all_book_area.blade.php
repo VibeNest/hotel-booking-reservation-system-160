3b11c67d8a78c3e882d59daf61e24af1c8f5bef9
vutungvn <tungvuvanthanh@gmail.com>
2026-04-13 21:16:09 +0700

    feat(book_area): show data field in db of book area management

---
+++ resources/views/backend/book_area/all_book_area.blade.php
@@ -0,0 +1,64 @@
+@extends("admin.admin_dashboard")
+
+@section("admin")
+    <div class="page-content">
+        <!--breadcrumb-->
+        <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+            <div class="breadcrumb-title pe-3">All Book Area</div>
+            <div class="ps-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0 p-0">
+                        <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-building-house"></i></a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">All Book Area</li>
+                    </ol>
+                </nav>
+            </div>
+            <div class="ms-auto">
+                <div class="btn-group">
+                    <a href="{{ route('add.book.area') }}" class="btn btn-primary px-3"><i class="bx bx-plus me-1"></i>Add
+                        Book Area</a>
+                </div>
+            </div>
+        </div>
+        <!--end breadcrumb-->
+        <h6 class="mb-0 text-uppercase">All Book Area</h6>
+        <hr />
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="example" class="table table-striped table-bordered" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Sl</th>
+                                <th>Sub_title</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th>Link_url</th>
+                                <th>Image</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($bookArea as $key => $item)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td>{{ $item->sub_title }}</td>
+                                    <td>{{ $item->title }}</td>
+                                    <td>{{ $item->description }}</td>
+                                    <td>{{ $item->link_url }}</td>
+                                    <td><img src="{{ asset($item->image) }}" alt="{{ $item->sub_title }}"
+                                            style="width: 70px; height: 40px;"></td>
+                                    <td>
+                                        <a href="" class="btn btn-warning px-3 radius-30">Edit</a>
+                                        <a href="" class="btn btn-danger px-3 radius-30" id="delete">Delete</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

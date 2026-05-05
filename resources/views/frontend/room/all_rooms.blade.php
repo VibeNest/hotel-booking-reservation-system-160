2d3c41ab6d8a2e252e4228d4a1235f89527e96fd
aye2710 <xongtruc2004@gmail.com>
2026-05-05 23:55:01 +0700

    done phantrang

--- resources/views/frontend/room/all_rooms.blade.php
+++ resources/views/frontend/room/all_rooms.blade.php
@@ -53,19 +53,7 @@
                 @endforeach
 
                 <div class="col-lg-12 col-md-12">
-                    <div class="pagination-area">
-                        <a href="#" class="prev page-numbers">
-                            <i class='bx bx-chevrons-left'></i>
-                        </a>
-
-                        <span class="page-numbers current" aria-current="page">1</span>
-                        <a href="#" class="page-numbers">2</a>
-                        <a href="#" class="page-numbers">3</a>
-
-                        <a href="#" class="next page-numbers">
-                            <i class='bx bx-chevrons-right'></i>
-                        </a>
-                    </div>
+                    {{ $rooms->links('pagination::bootstrap-5') }}
                 </div>
             </div>
         </div>

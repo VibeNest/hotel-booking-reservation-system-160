de9ba34b62907c36eba71533d479085f8d39234d
VibeNest <nikich74am@gmail.com>
2026-07-03 18:10:13 +0300

    vibenest: sanitize docker-compose (strip host port bindings for managed deploy)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'roles' => Role::class,
             'admin.guest' => RedirectIfAdmin::class,
